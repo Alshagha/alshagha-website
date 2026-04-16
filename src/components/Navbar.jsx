b8619660710f7f4cd18a43412f99bfd8dac563ad
Alshagha <AlshaghaFamily@gmail.com>
2026-04-16 13:43:16 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = ({ scrolled }) => {
 
                 <div className={`navbar-caravan ${menuOpen ? 'hidden' : ''}`}>
                     {/* Using the custom image the user will provide */}
-                    <img src="/images/caravan.png" alt="قافلة" className="caravan-img" />
-                    <img src="/images/caravan.png" alt="قافلة" className="caravan-img" />
+                    <img src="/images/caravan.png" alt="قافلة" className="caravan-img" onError={(e) => e.target.style.opacity = 0} />
+                    <img src="/images/caravan.png" alt="قافلة" className="caravan-img" onError={(e) => e.target.style.opacity = 0} />
                 </div>
 
                 <button
@@ -28,7 +28,6 @@ const Navbar = ({ scrolled }) => {
                     <li><a href="#history" onClick={() => setMenuOpen(false)}>تاريخ الأسرة</a></li>
                     <li><a href="#figures" onClick={() => setMenuOpen(false)}>أعلام الأسرة</a></li>
                     <li><a href="#tree" onClick={() => setMenuOpen(false)}>شجرة الأسرة</a></li>
-                    <li><a href="#platform" onClick={() => setMenuOpen(false)}>المنصة الرقمية</a></li>
                     <li><a href="#council" onClick={() => setMenuOpen(false)}>مجلس الأسرة</a></li>
                     <li><a href="#brand-mark" onClick={() => setMenuOpen(false)}>وسم آل حمود</a></li>
                 </ul>
